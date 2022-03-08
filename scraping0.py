3794bc8e5ee961064932598d6c70ace99b9c4f1e
Aastha <34710946+aasthas97@users.noreply.github.com>
2022-03-08 14:11:01 +0530

    Add files via upload

--- scraping0.py
+++ scraping0.py
@@ -1,8 +1,6 @@
-"""Script to enter the location of a house and figure out how far it is from my place of work in Berlin."""
+"""Script to enter the name of a locality and find how far it is from Friedrichshain."""
 
 import webbrowser
-
-# https://www.google.com/maps/dir/Mitte,+Berlin,+Germany/eemagine+Medical+Imaging+Solutions+GmbH,+Gubener+Stra%C3%9Fe,+Berlin,+Germany/
 
 DESTINATION = 'eemagine Medical Imaging Solutions GmbH, Gubener Str. 47, 10243 Berlin, Germany'
 
@@ -10,5 +8,6 @@ def how_far_is_the_house(house_location):
     maps_url = 'https://www.google.com/maps/dir/%s/%s' % (house_location, DESTINATION)
     webbrowser.open(maps_url)
 
-loc = 'La Femme Patisserie'
+print('LOCALITY:')
+loc = input('')
 how_far_is_the_house(loc)
