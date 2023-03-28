5da567c3a7c157790d4b934c5156cbfa63812145
Aastha <34710946+aasthas97@users.noreply.github.com>
2023-03-28 10:39:45 +0200

    Add xaml and cs files

---
+++ fileExplorerApp/HeaderToPackIconKindConverter.cs
@@ -0,0 +1,36 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FileExplorer
+{
+    [ValueConversion(typeof(string), typeof(PackIconKind))]
+    class HeaderToPackIconKindConverter : IValueConverter
+    {
+        public static HeaderToPackIconKindConverter Instance = new HeaderToPackIconKindConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string filePath = (string)value;
+            
+            if (filePath == null)
+            { return null; }
+
+
+            //detect whether its a directory or file
+            FileAttributes attr = File.GetAttributes(filePath);
+
+            // TODO: Display different icons for collapsed and uncollapsed items
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                return PackIconKind.Folder;
+            else
+                return PackIconKind.FileDocumentOutline;
+        }
+        public object ConvertBack(object value, Type type, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
